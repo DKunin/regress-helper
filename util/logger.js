7b65bfe0ae859df39ffe877bb00e03ce32d7883b
Dmitry Kunin <dkunin@avito.ru>
2017-12-26 13:50:41 +0300

    Restore

--- util/logger.js
+++ util/logger.js
@@ -1,16 +1,16 @@
-// const bunyan = require('bunyan');
-// const bsyslog = require('bunyan-syslog-udp');
+const bunyan = require('bunyan');
+const bsyslog = require('bunyan-syslog-udp');
 
 module.exports = config => {
-    // const logger = bunyan.createLogger({ name: 'workshop' });
-    // const udbConfig = config.get('logger');
-    // // logger streams, add to array
+    const logger = bunyan.createLogger({ name: 'workshop' });
+    const udbConfig = config.get('logger');
+    // logger streams, add to array
 
-    // logger.addStream({
-    //     type: 'raw',
-    //     level: config.get('logger.level'),
-    //     stream: bsyslog.createBunyanStream(udbConfig)
-    // });
+    logger.addStream({
+        type: 'raw',
+        level: config.get('logger.level'),
+        stream: bsyslog.createBunyanStream(udbConfig)
+    });
 
-    return console;
+    return logger;
 };
